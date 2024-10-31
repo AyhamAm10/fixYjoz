b5f45a74677b0ea70dab2f82f3d2f8b4477298b1
kasem1234357 <kasemalolo2002@gmail.com>
2024-10-31 23:20:17 +0200

    fix homepage style mobile version

--- src/component/homePage/HeroSection.tsx
+++ src/component/homePage/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection:React.FC = () => {
     <section className=" flex-center py-4 [padding-inline:min(3vw,60px)] max-w-[2044px] mx-auto">
         <div className=" relative px-1 sm:px-0 sm:w-[90%] m-auto flex flex-col sm:block">
             <img src={imgSlider} alt="img slider" className="z-10 sm:block float-end rounded-bl-[100px] sm:rounded-bl-3xl md:w-[70%]" />
-            <div className=" z-20 sm:absolute sm:top-1/2 sm:translate-y-[-50%] sm:rounded__hero_card sm:px-3 py-5 sm:p-7 md:p-9 sm:bg-white ">
+            <div className=" z-20 sm:absolute sm:top-1/2 sm:translate-y-[-50%] sm:rounded-tr-[100px] sm:px-3 py-5 sm:p-7 md:p-9 sm:bg-white ">
                 <div className=" sm:translate-y-0 mt-4 mr-4 md:max-w-[600px] 2xl:max-w-[730px]">
                     <p className="font-semibold  text-3xl sm:text-5xl md:text-6xl text-ofblack " >
                     <span className="text-gradient linear-gradient(90deg, #ff7a18, #af002d 70%)">{t("Discover")} </span>
